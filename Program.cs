a4624a644c6276e05b019983de24212570f4d3c8
Iain Jenkins <63315736+Inferno-Dragon@users.noreply.github.com>
2020-10-22 09:18:57 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Number_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int evenNumber;
+            int oddNumber;
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to your random number generator" +
+                   "          \nPlease select either number 1,2 or 3" +
+                   "          \n1. Even number: " +
+                   "          \n2. Odd number:" +
+                   "          \n3. Exit: ");
+
+                string userChoice = Console.ReadLine();
+
+                if(userChoice == "1")
+                {
+                    // Creates even number 
+                    evenNumber = rnd.Next(int.MaxValue / 2) * 2;
+                    Console.WriteLine(evenNumber);
+                }
+
+                else if(userChoice == "2")
+                {
+                    // Creates odd number
+                    oddNumber = rnd.Next(int.MaxValue / 2) * 2 + 1;
+                    Console.WriteLine(oddNumber);
+                }
+
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
